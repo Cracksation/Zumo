e280946eb68a3b0027e8d9050c8f19ff1615981c
computer <computer@computer-PineTrail.(none)>
2013-12-30 21:01:52 -0700

    Initial

---
+++ ZumoTest1.ino
@@ -0,0 +1,16 @@
+#include<ZumoMotors.h>
+#define LED 13
+
+void setup()
+{
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, HIGH);
+  delay(1000);
+  digitalWrite(LED, LOW);
+}
+
+void loop()
+{
+  
+  
+}
